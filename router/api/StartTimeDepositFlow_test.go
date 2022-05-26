3ab594bba8b0052f529a5a4870551b6da11a2502
宗正 胡 <hu.zheng@aladinbank.id>
2022-05-26 08:50:59 +0000

    Add test case

--- router/api/StartTimeDepositFlow_test.go
+++ router/api/StartTimeDepositFlow_test.go
@@ -4,31 +4,92 @@
  * @Last Modified by: Hugo
  * @Last Modified time: 2022-05-23 11:25:01
  */
-package api
+ package api
 
-import (
-	commonConfig "gitlab.com/bns-engineering/td/common/config"
-	logger "gitlab.com/bns-engineering/td/common/log"
-	"gitlab.com/bns-engineering/td/flow"
-	"gitlab.com/bns-engineering/td/service/mambuEntity"
-	"go.uber.org/zap"
-	"testing"
-)
-
-func init() {
-	conf := commonConfig.Setup("../../config.yaml")
-	logger.SetUp(conf)
-	zap.L().Info("===============Start Test Whole Flow==============")
-	flow.InitWorkflow()
-}
-
-func TestRunFlow(t *testing.T) {
-	t.Run("run flow by account", func(t *testing.T) {
-		RunFlow(&mambuEntity.TDAccount{
-			ID: "11249460359",
-		})
-
-		t.Log("run flow by account success")
-	})
-
-}
+ import (
+	 "testing"
+ 
+	 "github.com/gin-gonic/gin"
+	 commonConfig "gitlab.com/bns-engineering/td/common/config"
+	 logger "gitlab.com/bns-engineering/td/common/log"
+	 "gitlab.com/bns-engineering/td/flow"
+	 "gitlab.com/bns-engineering/td/service/mambuEntity"
+	 "go.uber.org/zap"
+ )
+ 
+ func init() {
+	 conf := commonConfig.Setup("../../config.yaml")
+	 logger.SetUp(conf)
+	 zap.L().Info("===============Start Test Whole Flow==============")
+	 flow.InitWorkflow()
+ }
+ 
+ 
+ func TestStartTDFlow(t *testing.T) {
+	 type args struct {
+		 c *gin.Context
+	 }
+	 tests := []struct {
+		 name string
+		 args args
+	 }{
+		 // TODO: Add test cases.
+	 }
+	 for _, tt := range tests {
+		 t.Run(tt.name, func(t *testing.T) {
+			 StartTDFlow(tt.args.c)
+		 })
+	 }
+ }
+ 
+ func TestRunFlow(t *testing.T) {
+	 type args struct {
+		 tmpTDAcc *mambuEntity.TDAccount
+	 }
+	 tests := []struct {
+		 name string
+		 args args
+	 }{
+		 // TODO: Add test cases.
+		 {
+			 name : "test Account : 11249460359, test Update Maturity Date",
+			 args : args{ 
+				 tmpTDAcc : &mambuEntity.TDAccount{
+					 ID: "11249460359",
+				 },
+			 },
+		 },
+		 {
+			 name : "test Account : 11747126703",
+			 args : args{ 
+				 tmpTDAcc : &mambuEntity.TDAccount{
+					 ID: "11747126703",
+				 },
+			 },
+		 },
+		 {
+			 name : "test Account : 11714744288",
+			 args : args{ 
+				 tmpTDAcc : &mambuEntity.TDAccount{
+					 ID: "11714744288",
+				 },
+			 },
+		 },
+		 {
+			 name : "test Account : 11842046986",
+			 args : args{ 
+				 tmpTDAcc : &mambuEntity.TDAccount{
+					 ID: "11842046986",
+				 },
+			 },
+		 },
+	 }
+	 for _, tt := range tests {
+		 t.Run(tt.name, func(t *testing.T) {
+			 RunFlow(tt.args.tmpTDAcc)
+		 })
+	 }
+	 t.Log("run flow by account success")
+ }
+ 
+ 
