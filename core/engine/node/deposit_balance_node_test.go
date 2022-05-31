0aba35479c824f8db5a53ba6f25f8414c0eab4dd
hugo <huzongzheng@gmail.com>
2022-05-31 18:09:25 +0800

    add test cases

--- core/engine/node/deposit_balance_node_test.go
+++ core/engine/node/deposit_balance_node_test.go
@@ -1,40 +1,40 @@
 // Package node
+
 // @author： Boice
-// @createTime：2022/5/26 18:08
+
+// @createTime：2022/5/26 11:07
+
 package node
 
 import (
+	"reflect"
+	"testing"
 	"gitlab.com/bns-engineering/td/common/config"
 	"gitlab.com/bns-engineering/td/common/log"
 	"go.uber.org/zap"
-	"reflect"
-	"testing"
 )
 
-func TestApplyProfitNode_Run(t *testing.T) {
-
+func TestDepositBalanceNode_Run(t *testing.T) {
 	config.Setup("./../../../config.yaml")
 	err := logger.SetUp(config.TDConf)
 	if err != nil {
 		zap.L().Error("logger init error", zap.Error(err))
 	}
 
-	type fields struct {
-		Node *Node
-	}
+
 	tests := []struct {
 		name    string
-		fields  fields
+		node    *DepositBalanceNode
 		want    INodeResult
 		wantErr bool
 	}{
 		{
-			name: "apply profit test: 11645631879",
-			fields: fields{
-				Node: &Node{
-					FlowId:    "testFlowID_11645631879_1",
+			name: "Deposit total balance test: 11645631879",
+			node: &DepositBalanceNode{
+				Node:&Node{
+					FlowId:    "testFlowID_11645631879_2",
 					AccountId: "11645631879",
-					NodeName:  "apply_profit_node",
+					NodeName:  "deposit_balance_node",
 				},
 			},
 			want:    ResultSuccess,
@@ -43,16 +43,13 @@ func TestApplyProfitNode_Run(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			node := &ApplyProfitNode{
-				Node: tt.fields.Node,
-			}
-			got, err := node.Run()
+			got, err := tt.node.Run()
 			if (err != nil) != tt.wantErr {
-				t.Errorf("ApplyProfitNode.Run() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("DepositBalanceNode.Run() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("ApplyProfitNode.Run() = %v, want %v", got, tt.want)
+				t.Errorf("DepositBalanceNode.Run() = %v, want %v", got, tt.want)
 			}
 		})
 	}
--- core/engine/node/withdraw_additional_profit_node_test.go
+++ core/engine/node/withdraw_additional_profit_node_test.go
@@ -1,40 +1,40 @@
 // Package node
+
 // @author： Boice
-// @createTime：2022/5/26 18:08
+
+// @createTime：2022/5/27 09:18
+
 package node
 
 import (
+	"reflect"
+	"testing"
+	
 	"gitlab.com/bns-engineering/td/common/config"
 	"gitlab.com/bns-engineering/td/common/log"
 	"go.uber.org/zap"
-	"reflect"
-	"testing"
 )
 
-func TestApplyProfitNode_Run(t *testing.T) {
-
+func TestWithdrawAdditionalProfitNode_Run(t *testing.T) {
 	config.Setup("./../../../config.yaml")
 	err := logger.SetUp(config.TDConf)
 	if err != nil {
 		zap.L().Error("logger init error", zap.Error(err))
 	}
 
-	type fields struct {
-		Node *Node
-	}
 	tests := []struct {
 		name    string
-		fields  fields
+		node    *WithdrawAdditionalProfitNode
 		want    INodeResult
 		wantErr bool
 	}{
 		{
-			name: "apply profit test: 11645631879",
-			fields: fields{
-				Node: &Node{
+			name: "Withdraw additional profit test: 11645631879",
+			node: &WithdrawAdditionalProfitNode{
+				Node:&Node{
 					FlowId:    "testFlowID_11645631879_1",
 					AccountId: "11645631879",
-					NodeName:  "apply_profit_node",
+					NodeName:  "withdraw_additional_profit_node",
 				},
 			},
 			want:    ResultSuccess,
@@ -43,16 +43,13 @@ func TestApplyProfitNode_Run(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			node := &ApplyProfitNode{
-				Node: tt.fields.Node,
-			}
-			got, err := node.Run()
+			got, err := tt.node.Run()
 			if (err != nil) != tt.wantErr {
-				t.Errorf("ApplyProfitNode.Run() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("WithdrawAdditionalProfitNode.Run() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("ApplyProfitNode.Run() = %v, want %v", got, tt.want)
+				t.Errorf("WithdrawAdditionalProfitNode.Run() = %v, want %v", got, tt.want)
 			}
 		})
 	}
